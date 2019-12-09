f169a68b54f46b9513a8221fbfa441fbeb0d6c40
rnfinnegan <robert.n.finnegan@gmail.com>
2019-12-09 13:33:01 +1100

    Upload testing code.

---
+++ CalculateMetrics.py
@@ -0,0 +1,125 @@
+"""
+Service to run calculate label similarity metrics
+
+Rob Finnegan
+December 9, 2019
+"""
+
+import SimpleITK as sitk
+import numpy as np
+
+settings = {
+    "labels": ["HEART", "LUNG_L", "LUNG_R", "SPINALCORD", "ESOPHAGUS"],
+    "manual_dir" : "./data",
+    "auto_dir" : "./output"
+}
+
+def surfaceMetrics(imFixed, imMoving, verbose=False):
+    """
+    HD, meanSurfDist, medianSurfDist, maxSurfDist, stdSurfDist
+    """
+    hausdorffDistance = sitk.HausdorffDistanceImageFilter()
+    hausdorffDistance.Execute(imFixed, imMoving)
+    HD = hausdorffDistance.GetHausdorffDistance()
+
+    meanSDList = []
+    maxSDList = []
+    stdSDList = []
+    medianSDList = []
+    numPoints = []
+    for (imA, imB) in ((imFixed, imMoving), (imMoving, imFixed)):
+
+        labelIntensityStat = sitk.LabelIntensityStatisticsImageFilter()
+        referenceDistanceMap = sitk.Abs(sitk.SignedMaurerDistanceMap(imA, squaredDistance=False, useImageSpacing=True))
+        movingLabelContour = sitk.LabelContour(imB)
+        labelIntensityStat.Execute(movingLabelContour,referenceDistanceMap)
+
+        meanSDList.append(labelIntensityStat.GetMean(1))
+        maxSDList.append(labelIntensityStat.GetMaximum(1))
+        stdSDList.append(labelIntensityStat.GetStandardDeviation(1))
+        medianSDList.append(labelIntensityStat.GetMedian(1))
+
+        numPoints.append(labelIntensityStat.GetNumberOfPixels(1))
+
+    if verbose:
+        print("        Boundary points:  {0}  {1}".format(numPoints[0], numPoints[1]))
+
+    meanSurfDist = np.dot(meanSDList, numPoints)/np.sum(numPoints)
+    maxSurfDist = np.max(maxSDList)
+    stdSurfDist = np.sqrt( np.dot(numPoints,np.add(np.square(stdSDList), np.square(np.subtract(meanSDList,meanSurfDist)))) )
+    medianSurfDist = np.mean(medianSDList)
+
+    resultDict = {}
+    resultDict['hausdorffDistance'] = HD
+    resultDict['meanSurfaceDistance'] = meanSurfDist
+    resultDict['medianSurfaceDistance'] = medianSurfDist
+    resultDict['maximumSurfaceDistance'] = maxSurfDist
+    resultDict['sigmaSurfaceDistance'] = stdSurfDist
+
+    return resultDict
+
+def volumeMetrics(imFixed, imMoving):
+    """
+    DSC, VolOverlap, FracOverlap, TruePosFrac, TrueNegFrac, FalsePosFrac, FalseNegFrac
+    """
+    arrFixed = sitk.GetArrayFromImage(imFixed).astype(bool)
+    arrMoving = sitk.GetArrayFromImage(imMoving).astype(bool)
+
+    arrInter = arrFixed & arrMoving
+    arrUnion = arrFixed | arrMoving
+
+    voxVol = np.product(imFixed.GetSpacing())/1000. # Conversion to cm^3
+
+    # 2|A & B|/(|A|+|B|)
+    DSC =  (2.0*arrInter.sum())/(arrFixed.sum()+arrMoving.sum())
+
+    #  |A & B|/|A | B|
+    FracOverlap = arrInter.sum()/arrUnion.sum().astype(float)
+    VolOverlap = arrInter.sum() * voxVol
+
+    TruePos = arrInter.sum()
+    TrueNeg = (np.invert(arrFixed) & np.invert(arrMoving)).sum()
+    FalsePos = arrMoving.sum()-TruePos
+    FalseNeg = arrFixed.sum()-TruePos
+
+    #
+    TruePosFrac = (1.0*TruePos)/(TruePos+FalseNeg)
+    TrueNegFrac = (1.0*TrueNeg)/(TrueNeg+FalsePos)
+    FalsePosFrac = (1.0*FalsePos)/(TrueNeg+FalsePos)
+    FalseNegFrac = (1.0*FalseNeg)/(TruePos+FalseNeg)
+
+    resultDict = {}
+    resultDict['DSC'] = DSC
+    resultDict['volumeOverlap'] = VolOverlap
+    resultDict['fractionOverlap'] = FracOverlap
+    resultDict['truePositiveFraction'] = TruePosFrac
+    resultDict['trueNegativeFraction'] = TrueNegFrac
+    resultDict['falsePositiveFraction'] = FalsePosFrac
+    resultDict['falseNegativeFraction'] = FalseNegFrac
+
+    return resultDict
+
+if __name__ == '__main__':
+
+    labels = settings['labels']
+    manual_dir = settings['manual_dir']
+    auto_dir = settings['auto_dir']
+
+    test_id = 'Test-S1-101'
+
+    # Read manual & auto structures
+    manual_struct = {i:sitk.ReadImage(f'{manual_dir}/{test_id}/Structures/{test_id}_{i}.nii.gz') for i in labels}
+    auto_struct = {i:sitk.ReadImage(f'{auto_dir}/{test_id}/{test_id}_{i}_AUTO.nii.gz') for i in labels}
+
+    data = []
+
+    # Compare labels
+    for i,struct in enumerate(labels):
+        volume_metrics = volumeMetrics(manual_struct[struct], auto_struct[struct])
+        surface_metrics = surfaceMetrics(manual_struct[struct], auto_struct[struct])
+
+        data.append([struct, volume_metrics['DSC'], surface_metrics['meanSurfaceDistance'], surface_metrics['hausdorffDistance']])
+
+
+    # Save to file
+    np.savetxt(f'Output-{test_id}.csv', data, fmt='%s', delimiter=',', newline='\n', header='struct,dsc,masd,hd')
--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # SimpleSeg
-Simple Atas-Based Segmentation
+
+An open-source, Python-based atlas-based segmentation framework.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. This was originally developed on Linux (Ubuntu), although getting it to work on Windows/Mac should be fairly straightforward.
+
+### Prerequisites
+
+Before starting, make sure you have Python (version 3) and a Python package manager (e.g. Pip, Conda). Next, you can install all of the required packages using the included list of Python packages.
+
+```
+pip install -r requirements-txt
+```
+
+### Included dataset
+
+We have included a dataset in this repository, accessed from The Cancer Imaging Archive:
+
+```
+Aerts, H. J. W. L., Wee, L., Rios Velazquez, E., Leijenaar, R. T. H., Parmar, C., Grossmann, P., … Lambin, P. (2019). Data From NSCLC-Radiomics [Data set]. The Cancer Imaging Archive. https://doi.org/10.7937/K9/TCIA.2015.PF0M9REI
+```
+
+This dataset consists of non-contrast CT imaging of 60 lung cancer patients, with five organ-at-risk contours (heart, left lung, right lung, spinal cord, esophagus).
+
+This collection may not be used for commercial purposes. This collection is freely available to browse, download, as outlined in the Attribution-NonCommercial 3.0 Unported (CC BY-NC 3.0) https://creativecommons.org/licenses/by-nc/3.0/. It is able to be shared and adapted. We have converted the original DICOM images and RT-STRUCT files into compressed NifTI files. We have also cropped the images to contain to the extent of the contours (to make deformable image registration more efficient).
+
+
+### Demonstration
+
+The SimpleSeg codebase is included (in the *code* directory), and a demonstration of how this code can be used for end-to-end segmentation is available in the *SimpleSeg.py* script.
+
+Have a read of the *SimpleSeg.py* script, most of the settings are made to be user-understandable. At the bottom of the script, the target image is defined.
+
+The script can be run in a terminal:
+
+```
+python SimpleSeg.py
+```
+
+### Measuring segmentation accuracy
+
+We include a simple script to generate quantitative label similarity metrics, this is useful to evaluate the performance of an automatic segmentation algorithm. To run this script, in a terminal:
+
+```
+python ComputeMetrics.py
+```
+
+# Additional information
+
+## Authors
+
+* **Robert Finnegan** - robert *dot* finnegan *at* sydney.edu.au
+* **Philip Chlap** - philip *dot* chlap *at* sydney.edu.au
+
+## Built With
+
+* [SimpleITK](adress) - The web framework used
+
+## Contributing
+
+You are welcome to contribute to this project!
+
+## Versioning
+
+## License
+
+This project is licensed under the GNU GENERAL PUBLIC LICENSE, see the [LICENSE](LICENSE) file.
+
+## Acknowledgments
